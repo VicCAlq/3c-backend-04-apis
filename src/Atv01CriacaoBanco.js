1d9f957596e6baa56d728ce8172f7e08eca3277d
Mike <izabella.cristine@nave.org.br>
2026-07-02 11:43:42 +0000

    Atv 1 feita

--- src/Atv01CriacaoBanco.js
+++ src/Atv01CriacaoBanco.js
@@ -24,3 +24,46 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+const porta = 3000;
+const express = require('express'),
+const req = require('express/lib/request')
+const app = express(),
+const path = require('path'),
+const sql = require('sqlite3').verbose(),
+const db = new sql.Database(
+  'beyblade.db', (erro) => {
+    if(erro){
+      console.log("Erro ao criar o banco de dados beyblade.db, ", erro.message)
+    } else{
+      console.log("Sucesso ao criar o banco de dados beyblade.db")
+    }
+  }
+)
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS beyblade(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT, NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )`, (erro) => {
+    if(erro){
+      console.log("Erro ao criar a tabela beyblade, ", erro.message)
+    } else {
+      console.log("Tabela criada com sucesso")
+    }
+  }
+)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.listen(porta, () => {
+  console.log("App rodando na porta ", porta)
+})
+
+module.exports = app
