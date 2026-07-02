22ebe7b4876fec31de002a5a9570eb701a1908bb
beatriz-carneiro <beatriz.carneiro@nave.org.br>
2026-07-02 09:08:04 -0300

    atv 3 incompleta

--- src/Atv03AcrescentarItem.js
+++ src/Atv03AcrescentarItem.js
@@ -17,3 +17,68 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+const sql = require('sqlite3').verbose()
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+const db = new sql.Database(
+  "./beyblade.db",
+  (erro) => { 
+    if (erro) {
+      console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
+    } else {
+      console.log('Conectado ao banco de dados SQLite3 "beyblade.db"');
+    }})
+
+db.run(`CREATE TABLE IF NOT EXISTS beyblades
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )
+  `,
+  (erro) => {
+    if (erro) {
+      console.error('Erro ao criar a tabela "selecao"', erro.message);
+    } else {
+      console.log('Tabela "selecao" pronta!');
+    }}
+)
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/api/beyblade', (req,res) =>{
+  db.all(
+    `SELECT * FROM beyblades`,
+    [],
+    (erro, itensBeyblade) => {
+       if (erro) {
+        res.status(400).json({ error: erro.message })
+        return
+      }
+
+      res.status(200).json({message: "Requisição feita com sucesso",
+        data: itensBeyblade})
+    })})
+
+app.get('/api/beyblade/cadastrar', (req,res) =>{
+  
+})
+
+
+app.listen(3000, () => {
+})
+
+module.exports = app
